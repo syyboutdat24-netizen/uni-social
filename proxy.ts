7724d7399e95f495458191c5ff787d291dd83ec8
Shu Morikawa <shumorikawa@Shus-MacBook-Air.local>
2026-04-21 12:38:40 +0800

    replace middleware with proxy

--- proxy.ts
+++ proxy.ts
@@ -7,13 +7,6 @@ export async function middleware(request: NextRequest) {
 
 export const config = {
   matcher: [
-    /*
-     * Match all request paths except:
-     * - _next/static (static files)
-     * - _next/image (image optimization)
-     * - favicon.ico
-     * - images in the public folder
-     */
     "/((?!_next/static|_next/image|favicon.ico|.*\\.(?:svg|png|jpg|jpeg|gif|webp)$).*)",
   ],
 };
